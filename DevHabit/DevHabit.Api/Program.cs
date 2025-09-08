bc976f5dd63e5fd6e8d4370606a2c5fea5e9d4b9
Mahidy Hasan <khandokarmahidyhasan@gmail.com>
2025-09-08 12:20:43 +0600

    Initial project setup with build properties, central package versioning, Docker, OpenTelemetry, and GitHub Actions workflow

---
+++ DevHabit/DevHabit.Api/Program.cs
@@ -0,0 +1,44 @@
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+    .WithTracing(tracing => tracing
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation())
+    .WithMetrics(metrix => metrix
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation()
+        .AddRuntimeInstrumentation())
+    .UseOtlpExporter();
+
+builder.Logging.AddOpenTelemetry(option =>
+{
+    option.IncludeScopes = true;
+    option.IncludeFormattedMessage = true;
+});
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+app.MapControllers();
+
+await app.RunAsync();
